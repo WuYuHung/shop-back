0afce78f9c1764d8d13a6d69f8907241ee968a5c
juck89724 <juck89724@gmail.com>
2018-12-07 15:51:16 +0800

    product 分頁

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -17,8 +17,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-        $products = Product::all();
-
+        $products = Product::paginate(20,null,'page');
         return response()->json($products,200);
     }
 
